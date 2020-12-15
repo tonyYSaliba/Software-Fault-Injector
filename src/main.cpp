65277f74e135319a8499877d4dc4cc6ed73dee76
TONI SALIBA s275112 <s275112@studenti.polito.it>
2020-12-15 22:51:03 +0000

    Add execute_debugee

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,14 @@
 
 #include "linenoise.h"
 
+void execute_debugee (const std::string& prog_name) {
+    if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) {
+        std::cerr << "Error in ptrace\n";
+        return;
+    }
+    execl(prog_name.c_str(), prog_name.c_str(), nullptr);
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         std::cerr << "Please specify the name of the program that you want to debug.\nExiting...\n";
