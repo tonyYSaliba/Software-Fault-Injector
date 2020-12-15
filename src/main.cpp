62b0a1dd4ff45085caca3a748621b9835f974185
TONI SALIBA s275112 <s275112@studenti.polito.it>
2020-12-15 22:37:46 +0000

    Add libraries

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <sys/personality.h>
+#include <unistd.h>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+#include "linenoise.h"
