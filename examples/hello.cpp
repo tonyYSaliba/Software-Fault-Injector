f55d48086b61f64ccbf3408b2f8809b3c0d7f446
TONI SALIBA s275112 <s275112@studenti.polito.it>
2021-01-07 14:48:47 +0000

    Edit project skeleton

--- examples/hello.cpp
+++ examples/hello.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
 
 int main () {
-            std::cout<<"hello world 1!\n";
-
-//    try {
-
-//     //    int a = 0;
-//     //    int b = 10/a;
-//         throw "eeeee";
-//         std::cout<<"hello world 1!\n";
-
-//    } catch (const char* msg) {
-//         std::cout<<"hello world 2!\n";
-//    }
-
+    std::cerr << "Hello world";
 }
